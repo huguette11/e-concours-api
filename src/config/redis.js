3f67cedc4bdfef7903d1a5dc1a99250849eccd94
Yobi <yobibah7295@gmail.com>
2026-04-08 16:54:28 +0000

    config docker & redis

--- src/config/redis.js
+++ src/config/redis.js
@@ -3,4 +3,7 @@ import { Redis } from "ioredis";
 export const connection = new Redis({
   host: "127.0.0.1",
   port: 6379,
-}); 
+});
+
+connection.on("connect", () => console.log(" Redis connecté"));
+connection.on("error", (err) => console.error("Redis erreur :", err));
