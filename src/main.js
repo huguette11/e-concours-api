14bb3c6aeedf7c6884f73b002db8272a4f648fb2
Yobi <yobibah7295@gmail.com>
2026-04-24 09:42:45 +0000

    fix mailer

--- src/main.js
+++ src/main.js
@@ -20,28 +20,28 @@ const app = express();
 
 const PORT = process.env.PORT || 4000;
 
-const allowedOrigins = [
-  "http://localhost:3000",   
-  "http://localhost:4000",  
-  "http://localhost:5000",    
-  "http://localhost:6000", 
-];
+// const allowedOrigins = [
+//   "http://localhost:3000",   
+//   "http://localhost:4000",  
+//   "http://localhost:5000",    
+//   "http://localhost:6000", 
+// ];
 
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
+// const corsOptions = {
+//   origin: (origin, callback) => {
+//     if (!origin) return callback(null, true);
 
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("CORS non autorisé"));
-    }
-  },
-  credentials: true,
-};
+//     if (allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("CORS non autorisé"));
+//     }
+//   },
+//   credentials: true,
+// };
 await ensureBucketExists('e-concours');
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(helmet());
 app.use(express.json());
 app.use(limiter)
